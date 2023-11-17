a7505001c7666a6e7395c0c06adb9ed558fc421f
RangaSahithi <141542612+RangaSahithi@users.noreply.github.com>
2023-11-17 19:26:39 +0530

    Add files via upload

---
+++ studentbio/Web Page using Bootstrap/index13.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+  <title>SRIT</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <header class="header">
+    <nav class="navbar navbar-style">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#micon">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href=""><img style=float:left class="logo" src="srit_logo.jpg" ></a>
+        </div>
+        <div class ="collapse navbar-collapse" id="micon">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="">Deparments</a></li>
+            <li><a href="">Contact us</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <dl align="justify">
+            <dt><b>VISION</b></dt>
+            <dd><ul><li>To become a premier Educational Institution in India offering the best teaching and learning environment for our students that will enable them become complete individuals with professional competency, human touch, ethical values, service motto, and a strong sense of responsibility towards environment and society at large.</li></ul></dd>
+            <dt><b>MISSION</b></dt>
+            <dd><ul>
+              <li>Continually enhance the quality of physical infrastructure and human resources to evolve in to a centre of excellence in engineering education</li>
+              <li>Provide comprehensive learning experiences that are conducive for the students to acquire professional competences, ethical values, life-long learning abilities and understanding of the technology, environment and society.</li>
+              <li>Strengthen industry institute interactions to enable the students work on realistic problems and acquire the ability to face the ever changing requirements of the industry.</li>
+              <li>Continually enhance the quality of the relationship between students and faculty which is a key to the development of an exciting and rewarding learning environment in the college.</li>
+            </ul></dd>
+          </dl>
+        </div>
+        <div class="col-sm-6">
+          <img src="srit_internshala.jpeg">
+        </div>
+      </div>
+    </div>
+  </header><br><br>
+  <div id = "footer">
+    <p align = "center"> &copy; Venkatesh K--all rights-reserved </p>
+  </div>
+</body>
+</html>
